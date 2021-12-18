2cc60962630350626b15d30831cbd5619655cb87
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2021-12-17 20:19:07 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { pool } = require('./config');
+
+// App config
+const app = express();
+const port = process.env.PORT || 8001;
+
+// Middlewares
+app.use(express.json());
+
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://bug-tracker-backend-jy.herokuapp.com/' : '*',
+}
+app.use(cors(origin));
+
+// DB config
+
+// API endpoints
+const getData = (req, res) => {
+  pool.query('SELECT * FROM bugs', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  });
+}
+
+app.get('/', (req, res) => res.status(200).send('Hello'));
+
+app
+  .route('/data')
+  .get(getData);
+
+// Listener
+app.listen(port, () => console.log(`Listening. Port: ${port}`));
