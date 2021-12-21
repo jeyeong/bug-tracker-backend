72428a14b7bc50a94b1a439450cb9aad4db3643e
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2021-12-21 15:22:16 -0600

    Added put route to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,52 @@
 const usersRouter = require('express').Router();
+const { response } = require('express');
 const { pool } = require('../config/config');
 
 usersRouter.get('/', (req, res) => {
-  pool.query('SELECT * FROM users', (error, results) => {
-    if (error) {
-      throw error;
+  pool.query(
+    'SELECT * FROM users ORDER BY user_id',
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
     }
-    res.status(200).json(results.rows);
-  });
+  );
 });
 
 usersRouter.get('/:id', (req, res) => {
-  pool.query(`SELECT * FROM users WHERE user_id = '${req.params.id}'`, (error, results) => {
-    if (error) {
-      throw error;
+  const id = req.params.id;
+
+  pool.query(
+    'SELECT * FROM users WHERE user_id = $1',
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows[0]);
     }
-    res.status(200).json(results.rows[0]);
-  });
+  );
+})
+
+usersRouter.put('/', (req, res) => {
+  const { user_id, role } = req.body;
+
+  if (role === 'Admin') {
+    res.status(400).json({error: 'Cannot set admin'});
+    return;
+  }
+
+  pool.query(
+    'UPDATE users SET role = $1 WHERE user_id = $2',
+    [role, user_id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`User modified with ID: ${user_id}`);
+    }
+  )
 })
 
 module.exports = usersRouter;
