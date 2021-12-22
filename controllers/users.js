5750e9ed3da64dd4f025611c21234c6d22d5d61a
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2021-12-21 21:15:46 -0600

    Added unassigned users route

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,4 @@
 const usersRouter = require('express').Router();
-const { response } = require('express');
 const { pool } = require('../config/config');
 
 usersRouter.get('/', (req, res) => {
@@ -27,7 +26,7 @@ usersRouter.get('/:id', (req, res) => {
       res.status(200).json(results.rows[0]);
     }
   );
-})
+});
 
 usersRouter.put('/', (req, res) => {
   const { user_id, role } = req.body;
@@ -47,6 +46,18 @@ usersRouter.put('/', (req, res) => {
       res.status(200).send(`User modified with ID: ${user_id}`);
     }
   )
-})
+});
+
+usersRouter.get('/unassigned', (req, res) => {
+  pool.query(
+    'SELECT * FROM unassigned_users ORDER BY user_id',
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+});
 
 module.exports = usersRouter;
