f59ddcdc9fafa7a6325dfad9e5b7c8337aca497b
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2021-12-24 20:16:28 -0600

    Updated project creation to return project_id

--- controllers/projects.js
+++ controllers/projects.js
@@ -26,13 +26,13 @@ projectRouter.post('/', (req, res) => {
   const { name, description } = req.body;
 
   pool.query(
-    'INSERT INTO projects (name, description) VALUES ($1, $2)',
+    'INSERT INTO projects (name, description) VALUES ($1, $2) RETURNING project_id',
     [name, description],
     (error, results) => {
       if (error) {
         throw error;
       }
-      res.status(200).send(`Project created.`);
+      res.status(200).json(results.rows[0]);
     }
   )
 })
