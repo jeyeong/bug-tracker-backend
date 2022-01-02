750586e29abbc0c2456bb0d38011f1294c979fbf
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-01-01 23:06:33 -0600

    Rebuild backend

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use(compression());
 app.use(cors({ origin: isProduction ? '*' : '*' }));
 
 // Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
 
 // API endpoints
 const usersRouter = require('./controllers/users');
